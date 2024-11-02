4ac9ae09cc5c4b094bc41512b8d926588702a4a2
Jesús Benavides <nassicand@gmail.com>
2024-11-02 02:04:03 -0500

    Refactor Home.js: Adjust layout and styling in Home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -43,7 +43,7 @@ const Home = ({ onCourseSelect }) => {
       .replace(/\.[^/.]+$/, ""); // Remover la extensión
 
     return (
-      <div className="mb-8 bg-white p-6 rounded-lg shadow-md">
+      <div className="mb-6 bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-xl font-bold mb-4">Continuar viendo</h2>
         <div className="flex items-center justify-between">
           <div className="flex items-center">
@@ -110,7 +110,7 @@ const Home = ({ onCourseSelect }) => {
   return (
     <div className="p-8 bg-gray-100">
       <div className="max-w-screen-xl mx-auto">
-        <div className="flex flex-col mb-6">
+        <div className="flex flex-col">
           <h1 className="text-3xl font-bold mb-6">Cursos disponibles</h1>
           <div className="relative">
             <input
@@ -135,7 +135,7 @@ const Home = ({ onCourseSelect }) => {
               />
             </svg>
           </div>
-          {renderLastWatched()}
+          <div className="flex flex-col pt-4">{renderLastWatched()}</div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredCourses.map((course) => {
